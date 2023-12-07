65ec4f125386366a61b2f8f8780396fcdd964272
Pratham-22 <88884985+Pratham-22@users.noreply.github.com>
2023-12-07 18:31:48 +0530

    Css

---
+++ App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Common from "./pages/Common/Common";
+import AvailableShops from "./pages/User/AvailableShops/AvailableShops";
+import Slots from "./pages/User/Slots/Slots";
+import Login from "./components/Login/Login";
+import LoginPage from "./pages/LoginPage/Login";
+import SlotsTable from "./components/Slotstables/SlotTable";
+import AddSlotPage from "./pages/Admin/AddSlot/AddSlotPage";
+import AdminLogin from "./pages/Admin/Login/AdminLogin";
+
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    background: {
+      default: "#081220",
+      paper: "#081220",
+    },
+    primary: {
+      main: "#1799E1",
+    },
+    secondary: {
+      main: "#1799E1",
+    },
+    // whitetext: {
+    // 	main: "#fafafa",
+    // },
+    // pureWhite: {
+    // 	main: "#FFFFFF",
+    // },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  }
+});
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/"
+              component={Common} />
+            <Route path="/availableshops"
+              component={AvailableShops} />
+            <Route path="/viewslots"
+              component={Slots} />
+            <Route path="/userlogin"
+              component={LoginPage} />
+            <Route path="/adminlogin"
+              component={AdminLogin} />
+            <Route path="/admin/home"
+              component={AddSlotPage} />
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
